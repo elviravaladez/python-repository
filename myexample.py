2ee0d10c8a50c1147f12fd12e18597c123499666
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-15 18:54:09 -0500

    add: variable assignment practice

--- myexample.py
+++ myexample.py
@@ -1,2 +1,34 @@
 print('hello world')
 print('new')
+
+a = 5
+print(a)
+
+a = 10
+print(a)
+
+print(a + a)
+# 20
+
+a = a + a
+
+print(a)
+# 20
+
+print(type(a))
+# <class 'int'>
+
+
+# Don't use built-in key words, like "int" in Python
+# Example Below:
+# int = 4
+
+my_income = 500
+
+tax_rate = 0.1
+
+my_taxes = my_income * tax_rate
+
+print(my_taxes)
+# 50.0
+
