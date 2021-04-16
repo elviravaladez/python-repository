5903c74f00ee02918b0fd7bfb4730a272c7444f9
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-15 19:20:14 -0500

    add: strings intro

---
+++ strings-intro.py
@@ -0,0 +1,31 @@
+# Strings are sequences of characters, using the syntax of either single or double quotes
+# Example:
+# 'hey'
+# "Hey"
+# " I don't just like Python. I love it! "
+
+# Strings are ORDERED SEQUENCES
+# Uses indexing and slicing to grab sub-sections of the string
+
+# These actions use [] square brackets and a number index to indicate postions of what you wish to grab
+
+#     Character: h  e  l  l  o
+#         Index: 0  1  2  3  4
+# Reverse Index: 0 -4 -3 -2 -1
+
+print('hello')
+
+print("world")
+
+print('this is also a string')
+
+print(" I'm going on a run ")
+
+print("hello friend one(:")
+
+print("hello friend two(:")
+
+print('hello \nworld')
+
+print(len("hello"))
+# 5
