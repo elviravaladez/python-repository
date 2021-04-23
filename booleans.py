570a2a866055ef4b2adc7777b74bdf476ec62176
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-23 00:30:15 -0500

    add: booleans

---
+++ booleans.py
@@ -0,0 +1,18 @@
+print(True)
+# True
+print(type(True))
+# <class 'bool'>
+
+print(False)
+# False
+print(type(False))
+# <class 'bool'>
+
+print(1 > 2)
+# False
+
+print(1 == 1)
+# True
+
+b = None
+# This makes b a "None" data type
